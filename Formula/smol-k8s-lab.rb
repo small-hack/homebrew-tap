81e41b5fd98140cab687018d7e1342abf64396e6
jessebot <jessebot@linux.com>
2024-01-09 17:20:33 +0000

    Updating latest package from v2.0.0b7 to v2.0.0b8

--- Formula/smol-k8s-lab.rb
+++ Formula/smol-k8s-lab.rb
@@ -3,7 +3,7 @@
 class SmolK8sLab < Formula
   desc "Bootstrap simple projects on Kubernetes with kind, k3d, and k3s. This is a beta release"
   homepage "https://small-hack.github.io/smol-k8s-lab"
-  url "https://github.com/small-hack/smol-k8s-lab/archive/refs/tags/v2.0.0b7.tar.gz"
+  url "https://github.com/small-hack/smol-k8s-lab/archive/refs/tags/v2.0.0b8.tar.gz"
   sha256 "c4cd30c326f750b1b28e981163f3c97966625990b9ceb9c90b8fbc477fe15820"
   license "AGPL-3.0"
 
